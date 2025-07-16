36e843f4ba12106381c298fba6603d04f8ec009b
Ziang Ren <sandman_ren@outlook.com>
2025-07-16 04:24:49 -0400

    feat: Enhance theme transition durations and improve theme toggle animations

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -9,7 +9,7 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
       attribute="class"
       defaultTheme="system"
       enableSystem
-      disableTransitionOnChange={true}
+      disableTransitionOnChange={false}
       storageKey="biaori-vocab-theme"
       enableColorScheme={false}
       {...props}
--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -37,16 +37,16 @@ export function ThemeToggle() {
     switch (theme) {
       case 'light':
         return (
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-300 dark:-rotate-90 dark:scale-0" />
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-1000 dark:-rotate-90 dark:scale-0" />
         );
       case 'dark':
         return (
-          <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all duration-300 dark:rotate-0 dark:scale-100" />
+          <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all duration-1000 dark:rotate-0 dark:scale-100" />
         );
       case 'system':
       default:
         return (
-          <Monitor className="h-[1.2rem] w-[1.2rem] transition-all duration-300 theme-icon" />
+          <Monitor className="h-[1.2rem] w-[1.2rem] transition-all duration-1000 theme-icon" />
         );
     }
   };
